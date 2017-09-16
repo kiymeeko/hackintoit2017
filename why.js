696313b74255936082f3d4cbd4dc59ad35f8acb2
Jackson <jackson@betterweekdays.com>
2017-09-15 21:04:14 -0500

    amazon modal functionality starting

--- why.js
+++ why.js
@@ -1,3 +1,15 @@
+function modalCode() {
+  return  `
+  <div id="my_popup">
+    <p>Content!</p>
+  </div>
+`;
+}
+
+$('body').append(modalCode());
+
+
+
 const elementsToRunPopupOn = [
     '.sc-proceed-to-checkout'
 ];
@@ -6,13 +18,13 @@ const wrapClassName = 'asdfghjklqwertyuiop';
 
 function onCheckout(event) {
     event.preventDefault();
-
+    $('#my_popup').popup();
     console.log('running');
 }
 
 window.onload = function() {
     for (var i = 0; i < elementsToRunPopupOn.length; i++) {
-        $(elementsToRunPopupOn[i]).wrap("<div class='" + wrapClassName + "'></div>");
+        $(elementsToRunPopupOn[i]).wrap("<div class='" + wrapClassName + " my_popup_open'></div>");
     }
     $('.' + wrapClassName).one('click', onCheckout);
 };
